033ab172602d0a5cf7102cc3d9f7a8272c0ee987
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2022-11-30 22:27:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,18 @@ This app predicts the largest among 3 given numbers
 
 st.header('Input Parameters')
 
-num1 = st.number_input("Enter first number: ")
-num2 = st.number_input("Enter second number: ")
-num3 = st.number_input("Enter third number: ")
+def user_input_features():
+ num1 = st.number_input("Enter first number: ")
+ num2 = st.number_input("Enter second number: ")
+ num3 = st.number_input("Enter third number: ")
  
-if (num1 > num2) and (num1 > num3):
-   largest = num1
-elif (num2 > num1) and (num2 > num3):
-   largest = num2
-else:
-   largest = num3
  
-st.print("The largest number is",largest)
+ if (num1 > num2) and (num1 > num3):
+  largest = num1
+ elif (num2 > num1) and (num2 > num3):
+  largest = num2
+ else:
+  largest = num3
+  return("The largest number is",largest)
 
 
