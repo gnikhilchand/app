53a83a8959624690cd8a6d4aeb67105c5a1f0407
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2022-11-30 21:55:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# Finding largest among 3 given numbers
+This app predicts the largest among 3 given numbers
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def maximum(a, b, c): 
+   list = [a, b, c] 
+   return max(list) 
+# Driven code  
+x = int(input("Enter First number"))
+y = int(input("Enter Second number"))
+z = int(input("Enter Third number"))
+print("Maximum Number is ::>",maximum(x, y, z)) 
+
+
