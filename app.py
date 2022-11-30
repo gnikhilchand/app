5abd46e94ee1c8c61b9996ae0f9e352e10ae7108
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2022-11-30 22:41:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,15 @@ num1 = st.number_input("Enter first number: ")
 num2 = st.number_input("Enter second number: ")
 num3 = st.number_input("Enter third number: ")
  
- 
-st.code(if (num1 > num2) and (num1 > num3):)
+code = '''(if (num1 > num2) and (num1 > num3):)
+  largest = num1
+elif (num2 > num1) and (num2 > num3):
+  largest = num2
+else:
+  largest = num3
+ print("The largest number is",largest)
+'''
+(if (num1 > num2) and (num1 > num3):)
   largest = num1
 elif (num2 > num1) and (num2 > num3):
   largest = num2
