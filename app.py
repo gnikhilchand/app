1bc5e9a1b1b8207c8c3eb6612a075276fb30b233
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2022-11-30 23:03:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,55 +10,45 @@ This app predicts the largest among 3 given numbers
 """)
 #Get Input
 
-#st.header('Input Parameters')
+# st.header('Enter Three Numbers')
 
-
-# num1 = st.number_input("Enter first number: ")
-# num2 = st.number_input("Enter second number: ")
-# num3 = st.number_input("Enter third number: ")
- 
-# code = '''(if (num1 > num2) and (num1 > num3):)
-#   largest = num1
-# elif (num2 > num1) and (num2 > num3):
-#   largest = num2
-# else:
-#   largest = num3
-#  print("The largest number is",largest)
-# '''
-#st.code(code, language='python')
-# if (num1 > num2) and (num1 > num3):
-#   st.largest = num1
-# elif (num2 > num1) and (num2 > num3):
-#   st.largest = num2
-# else:
-#   st.largest = num3
-# print("The largest number is",st.largest)
-
-st.header('Enter Three Numbers')
-
-def user_input_features():
+# def user_input_features():
     
-    number_1 = st.number_input("number_1")
-    number_2 = st.number_input("number_2")
-    number_3 = st.number_input("number_3")
+#     number_1 = st.number_input("number_1")
+#     number_2 = st.number_input("number_2")
+#     number_3 = st.number_input("number_3")
     
 
-    data = {'NUMBER_1': number_1,
-            'NUMBER_2': number_2,
-            'NUMBER_3': number_3
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
+#     data = {'NUMBER_1': number_1,
+#             'NUMBER_2': number_2,
+#             'NUMBER_3': number_3
+#             }
+#     features = pd.DataFrame(data, index=[0])
+#     return features
 
-df = user_input_features()
+# df = user_input_features()
 
-st.subheader('User Input parameters')
-st.write(df.to_dict())
+# st.subheader('User Input parameters')
+# st.write(df.to_dict())
 
 
 
-largest=df.max()
-st.subheader('Largest Number')
-st.write(largest)
+# largest=df.max()
+# st.subheader('Largest Number')
+# st.write(largest)
+
+st.set_page_config(page_title=title)
+
+st.title('Largest Number Finder')
+
+x = st.number_input('Enter first number')
+y = st.number_input('Enter second number')
+z = st.number_input('Enter third number')
+
+l=[x,y,z]
+if x == y == z:
+  st.write("The numbers are Equal")
+else:
+  st.write(max(l),"is the largest number")
 
 
